7edc1b174692059aa19bbfa6ef56664efca3e45e
singhsanket143 <singhsanket143@gmail.com>
2019-05-25 18:17:26 +0530

    Added Basic React-Router-Dom code with BrowserRouter including Route for each component

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -1,8 +1,26 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom"; // To implement React-Router Functionalities
 
+import StreamList from "./streams/StreamList";
+import StreamCreate from "./streams/StreamCreate";
+import StreamEdit from "./streams/StreamEdit";
+import StreamDelete from "./streams/StreamDelete";
+import StreamShow from "./streams/StreamShow";
 class App extends React.Component {
   render() {
-    return <div>App</div>;
+    return (
+      <div>
+        <BrowserRouter>
+          <div>
+            <Route path="/" exact component={StreamList} />
+            <Route path="/streams/new" exact component={StreamCreate} />
+            <Route path="/streams/edit" exact component={StreamEdit} />
+            <Route path="/streams/delete" exact component={StreamDelete} />
+            <Route path="/streams/show" exact component={StreamShow} />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
   }
 }
 
