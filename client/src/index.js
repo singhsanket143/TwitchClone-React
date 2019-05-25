66051a818e4b2343916e2ab8c6164d2746df1a96
singhsanket143 <singhsanket143@gmail.com>
2019-05-25 21:57:56 +0530

    Added redux devtools for debugging and visualization

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 
 import App from "./Components/App";
 import reducers from "./Reducers";
 
-const store = createStore(reducers); // To create the store object of redux
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware())); // To create the store object of redux
 
 ReactDOM.render(
   <Provider store={store}>
