51e6c01acd6d224c495dcebc684e80367dfecac1
singhsanket143 <singhsanket143@gmail.com>
2019-05-25 23:15:00 +0530

    Added Initial Setup Of Redux Form

--- client/src/Components/streams/StreamCreate.js
+++ client/src/Components/streams/StreamCreate.js
@@ -1,9 +1,37 @@
 import React from "react";
+import { Field, reduxForm } from "redux-form"; // Field is a component and reduxForm works the work similar to that of connect
 
 class StreamCreate extends React.Component {
+  renderInputTag = formProps => {
+    // console.log(formProps);
+    // return <input onChange={formProps.input.onChange} />
+    return (
+      <div className="field">
+        <label>{formProps.label}</label>
+        <input {...formProps.input} />
+      </div>
+    );
+  };
+
   render() {
-    return <div>StreamCreate</div>;
+    // console.log(this.props);
+    return (
+      <form className="ui form">
+        <Field
+          name="title"
+          component={this.renderInputTag}
+          label="Enter Title"
+        />
+        <Field
+          name="description"
+          component={this.renderInputTag}
+          label="Enter Description"
+        />
+      </form>
+    );
   }
 }
 
-export default StreamCreate;
+export default reduxForm({
+  form: "streamCreate"
+})(StreamCreate);
--- client/src/Reducers/index.js
+++ client/src/Reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from "redux"; // TO combine all the available reducers
 
 import AuthReducers from "./AuthReduers";
+import { reducer as formReducer } from "redux-form";
 
 export default combineReducers({
-  auth: AuthReducers
+  auth: AuthReducers,
+  form: formReducer
 });
