bc9ca456a34a287cfe7c1fe647f6e06dbc14e409
singhsanket143 <singhsanket143@gmail.com>
2019-05-26 03:01:59 +0530

    Added ActionListeners for Rest API

--- client/src/Actions/index.js
+++ client/src/Actions/index.js
@@ -15,7 +15,51 @@ export const signOut = userid => {
 };
 
 export const createStream = formValues => {
-  return async dispath => {
-    streams.post("/streams", formValues);
+  return async dispatch => {
+    const response = await streams.post("/streams", formValues);
+    dispatch({
+      type: "CREATE_STREAM",
+      payload: response.data
+    });
+  };
+};
+
+export const fetchStream = id => {
+  return async dispatch => {
+    const response = await streams.get(`/streams/${id}`);
+    dispatch({
+      type: "FETCH_STREAM",
+      payload: response.data
+    });
+  };
+};
+
+export const fetchStreams = () => {
+  return async dispatch => {
+    const response = await streams.get("/streams");
+    dispatch({
+      type: "FETCH_STREAMS",
+      payload: response.data
+    });
+  };
+};
+
+export const editStream = (id, formValues) => {
+  return async dispatch => {
+    const response = await streams.put(`/streams${id}`, formValues);
+    dispatch({
+      type: "EDIT_STREAM",
+      payload: response.data
+    });
+  };
+};
+
+export const deleteStream = (id, formValues) => {
+  return async dispatch => {
+    streams.delete("/streams", formValues);
+    dispatch({
+      type: "DELETE_STREAM",
+      payload: id
+    });
   };
 };
