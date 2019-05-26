dc2eaa02772f2d48984b01ad0635199853c4f61d
singhsanket143 <singhsanket143@gmail.com>
2019-05-26 09:30:18 +0530

    Associated getState object to action creators to add userId in Streams

--- client/src/Actions/index.js
+++ client/src/Actions/index.js
@@ -15,8 +15,9 @@ export const signOut = userid => {
 };
 
 export const createStream = formValues => {
-  return async dispatch => {
-    const response = await streams.post("/streams", formValues);
+  return async (dispatch, getState) => {
+    const { userId } = getState().auth;
+    const response = await streams.post("/streams", { ...formValues, userId });
     dispatch({
       type: "CREATE_STREAM",
       payload: response.data
