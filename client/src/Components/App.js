cc0ffa8fa4cfde38234744a135d376236ec0a255
singhsanket143 <singhsanket143@gmail.com>
2019-05-26 10:01:22 +0530

    Added custom history object for programmatic routing

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom"; // To implement React-Router Functionalities
+import { Router, Route } from "react-router-dom"; // To implement React-Router Functionalities
 
 import StreamList from "./streams/StreamList";
 import StreamCreate from "./streams/StreamCreate";
@@ -8,11 +8,12 @@ import StreamDelete from "./streams/StreamDelete";
 import StreamShow from "./streams/StreamShow";
 import Header from "./Header";
 import "../CSS/index.css";
+import history from "../history";
 class App extends React.Component {
   render() {
     return (
       <div className="parent-div ui container">
-        <BrowserRouter>
+        <Router history={history}>
           <div>
             <Header />
             <Route path="/" exact component={StreamList} />
@@ -21,7 +22,7 @@ class App extends React.Component {
             <Route path="/streams/delete" exact component={StreamDelete} />
             <Route path="/streams/show" exact component={StreamShow} />
           </div>
-        </BrowserRouter>
+        </Router>
       </div>
     );
   }
