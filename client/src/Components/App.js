7d09623a986cc60287dca3a9d34e6d118384431c
singhsanket143 <singhsanket143@gmail.com>
2019-05-27 06:51:07 +0530

    Added show stream component

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Router, Route } from "react-router-dom"; // To implement React-Router Functionalities
+import { Router, Route, Switch } from "react-router-dom"; // To implement React-Router Functionalities
 
 import StreamList from "./streams/StreamList";
 import StreamCreate from "./streams/StreamCreate";
@@ -16,11 +16,18 @@ class App extends React.Component {
         <Router history={history}>
           <div>
             <Header />
-            <Route path="/" exact component={StreamList} />
-            <Route path="/streams/new" exact component={StreamCreate} />
-            <Route path="/streams/edit/:id" exact component={StreamEdit} />
-            <Route path="/streams/delete/:id" exact component={StreamDelete} />
-            <Route path="/streams/show" exact component={StreamShow} />
+            <Switch>
+              {/* Switch is used To avoid the conflict of new and show route*/}
+              <Route path="/" exact component={StreamList} />
+              <Route path="/streams/new" exact component={StreamCreate} />
+              <Route path="/streams/edit/:id" exact component={StreamEdit} />
+              <Route
+                path="/streams/delete/:id"
+                exact
+                component={StreamDelete}
+              />
+              <Route path="/streams/:id" exact component={StreamShow} />
+            </Switch>
           </div>
         </Router>
       </div>
--- client/src/Components/streams/StreamShow.js
+++ client/src/Components/streams/StreamShow.js
@@ -1,9 +1,28 @@
 import React from "react";
+import { connect } from "react-redux";
+import { fetchStream } from "../../Actions";
 
 class StreamShow extends React.Component {
+  componentDidMount = () => {
+    this.props.fetchStream(this.props.match.params.id);
+  };
   render() {
-    return <div>StreamShow</div>;
+    if (!this.props.stream) {
+      return <div>Loading..</div>;
+    }
+    return (
+      <div>
+        <h1>{this.props.stream.title}</h1>
+        <h3>{this.props.stream.description}</h3>
+      </div>
+    );
   }
 }
 
-export default StreamShow;
+const mapStateToProps = (state, ownProps) => {
+  return { stream: state.streams[ownProps.match.params.id] };
+};
+export default connect(
+  mapStateToProps,
+  { fetchStream }
+)(StreamShow);
