56659063a99e3765e73076e53f80733ba363bc0c
singhsanket143 <singhsanket143@gmail.com>
2019-05-27 06:04:17 +0530

    Added Styling to the modal component

--- client/src/Components/Modal.js
+++ client/src/Components/Modal.js
@@ -1,11 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import history from "../history";
 
 class Modal extends React.Component {
   render() {
     return ReactDOM.createPortal(
-      <div className="ui dimmer modals visible active">
-        <div className="ui standard modal visible active">hyhy</div>
+      <div
+        onClick={() => history.push("/")}
+        className="ui dimmer modals visible active"
+      >
+        <div
+          onClick={e => e.stopPropagation()}
+          className="ui standard modal visible active"
+        >
+          <div className="header">Delete Stream</div>
+          <div className="content">
+            Are you sure you want to delete this stream?
+          </div>
+          <div className="actions">
+            <button className="ui primary button">Delete</button>
+            <button className="ui button">Cancel</button>
+          </div>
+        </div>
       </div>,
       document.querySelector("#modal")
     );
